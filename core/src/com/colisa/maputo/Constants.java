deb5a54917deb2e7de2aa3f5b517e61f181edf55
mtali <mtali.emmanuel@protonmail.com>
2017-07-30 18:54:06 +0300

    Implement play setting and background on menu screen

--- core/src/com/colisa/maputo/Constants.java
+++ core/src/com/colisa/maputo/Constants.java
@@ -5,7 +5,13 @@ import com.badlogic.gdx.Application;
 @SuppressWarnings("WeakerAccess")
 public class Constants {
 
-    public static final boolean DEBUG_FLAG_MENU_SCREEN = true;
+    public static final boolean DEBUG_FLAG_MENU_SCREEN = false;
     public static final int LOG_LEVEL = Application.LOG_DEBUG;
+
+    public static final String SKIN_MAPUTO_UI = "data/maputo-ui.json";
+    public static final String TEXTURE_MAPUTO_UI = "data/maputo-ui.pack.atlas";
+
+    public static final float VIEWPORT_GUI_WIDTH = 480.0f;
+    public static final float VIEWPORT_GUI_HEIGHT = 800.0f;
 
 }
--- core/src/com/colisa/maputo/mainmenu/MenuScreen.java
+++ core/src/com/colisa/maputo/mainmenu/MenuScreen.java
@@ -4,18 +4,29 @@ package com.colisa.maputo.mainmenu;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Stack;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.colisa.maputo.AbstractScreen;
+import com.colisa.maputo.Constants;
 import com.colisa.maputo.DirectedGame;
 
 public class MenuScreen extends AbstractScreen {
     private static final String TAG = MenuScreen.class.getName();
-    private Skin skinMaputo;
+    private Skin maputoSkin;
     private Skin skinLibgdx;
     private Stage stage;
+    private Button playButton;
+    private Button settingButton;
+    private Image imageBackground;
+    private ButtonClickListener listener;
 
     public MenuScreen(DirectedGame game) {
         super(game);
@@ -23,20 +34,26 @@ public class MenuScreen extends AbstractScreen {
 
     @Override
     public void show() {
-        stage = new Stage();
+        stage = new Stage(new StretchViewport(Constants.VIEWPORT_GUI_WIDTH, Constants.VIEWPORT_GUI_HEIGHT));
         Gdx.input.setInputProcessor(stage);
 
+        if (listener == null) listener = new ButtonClickListener();
         // rebuilding the stage including individual layers
-        skinMaputo = new Skin();
+        maputoSkin = new Skin(Gdx.files.internal(Constants.SKIN_MAPUTO_UI), new TextureAtlas(Constants.TEXTURE_MAPUTO_UI));
         skinLibgdx = new Skin();
         Stack stack = new Stack();
         stage.clear();
         stage.addActor(stack);
+        stack.setFillParent(true);
 
-        // + background
-        Table backgroundLayer = new Table();
-        // todo: add a background image
+        // building individual layers
+        Table backgroundLayer = buildBackgroundLayer();
+        Table controlsLayer = buildControlsLayer();
+
+        // assemble stage
         stack.add(backgroundLayer);
+        stack.add(controlsLayer);
+        stack.pack();
 
     }
 
@@ -47,6 +64,7 @@ public class MenuScreen extends AbstractScreen {
 
     @Override
     public void render(float deltaTime) {
+        Gdx.gl.glClearColor(0x64 / 255.0f, 0x95 / 255.0f, 0xed / 255.0f, 0xff / 255.0f);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         stage.act(deltaTime);
         stage.draw();
@@ -56,7 +74,7 @@ public class MenuScreen extends AbstractScreen {
     public void hide() {
         stage.dispose();
         skinLibgdx.dispose();
-        skinMaputo.dispose();
+        maputoSkin.dispose();
         Gdx.app.debug(TAG, "disposing menu screen");
     }
 
@@ -68,5 +86,40 @@ public class MenuScreen extends AbstractScreen {
     @Override
     public InputProcessor getInputProcessor() {
         return stage;
+    }
+
+
+    private Table buildControlsLayer() {
+        Table layer = new Table();
+        layer.setFillParent(true);
+        layer.center();
+        // + play button
+        playButton = new Button(maputoSkin, "play");
+        layer.add(playButton);
+        playButton.addListener(listener);
+        // + setting button
+        settingButton = new Button(maputoSkin, "setting");
+        layer.add(settingButton);
+        settingButton.addListener(listener);
+        if (Constants.DEBUG_FLAG_MENU_SCREEN) layer.debug();
+        return layer;
+    }
+
+    private Table buildBackgroundLayer() {
+        Table layer = new Table();
+        imageBackground = new Image(maputoSkin, "background");
+        layer.add(imageBackground);
+        return layer;
+    }
+
+    private class ButtonClickListener extends ChangeListener {
+        @Override
+        public void changed(ChangeEvent event, Actor actor) {
+            if (actor.equals(playButton)) {
+                Gdx.app.debug(TAG, "Play button clicked");
+            } else if (actor.equals(settingButton)) {
+                Gdx.app.debug(TAG, "Settings button clicked");
+            }
+        }
     }
 }

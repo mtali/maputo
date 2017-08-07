a9bcd29122894126f03a587f03589530291c4dee
mtali <mtali.emmanuel@protonmail.com>
2017-08-07 17:42:29 +0300

    Add controls background

--- core/src/com/colisa/maputo/screens/MenuScreen.java
+++ core/src/com/colisa/maputo/screens/MenuScreen.java
@@ -4,6 +4,7 @@ package com.colisa.maputo.screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.NinePatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.math.Interpolation;
 import com.badlogic.gdx.scenes.scene2d.Actor;
@@ -17,6 +18,7 @@ import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.scenes.scene2d.utils.NinePatchDrawable;
 import com.colisa.maputo.Assets;
 import com.colisa.maputo.DirectedGame;
 import com.colisa.maputo.transition.ScreenTransition;
@@ -29,6 +31,7 @@ public class MenuScreen extends BasicScreen {
     private Button highScoreButton;
     private Image balloonImage;
     private MenuButtonsListener menuListener;
+    private NinePatchDrawable controlsBackground;
 
     // Screens
 
@@ -44,6 +47,9 @@ public class MenuScreen extends BasicScreen {
     @Override
     protected void generateContent() {
         if (null == menuListener) menuListener = new MenuButtonsListener();
+        if (null == controlsBackground) controlsBackground = new NinePatchDrawable(new NinePatch(
+                SkinManager.getInstance().getSkin().getRegion("menu-background"), 10, 10, 10, 10
+        ));
         super.generateContent();
         createButtonsAndPositionContent();
     }
@@ -56,21 +62,32 @@ public class MenuScreen extends BasicScreen {
 
 
         Table innerTable = new Table();
+
+        // Layer to hold the buttons and dark background
+        Table buttonsTable = new Table();
+
         TextureAtlas.AtlasRegion region = Assets.instance.assetBalloon.balloon;
+
         balloonImage = new Image(region);
         playButton = new TextButton("Play", skin, "play");
         settingsButton = new TextButton("Setting", skin, "setting");
         highScoreButton = new TextButton("Leaders", skin, "leaders");
+
+        buttonsTable.add(playButton).width(UI.Buttons.TEXT_BUTTON_WIDTH).height(UI.Buttons.TEXT_BUTTON_HEIGHT).pad(UI.Buttons.SPACE);
+        buttonsTable.row();
+        buttonsTable.add(settingsButton).width(UI.Buttons.TEXT_BUTTON_WIDTH).height(UI.Buttons.TEXT_BUTTON_HEIGHT).pad(UI.Buttons.SPACE);
+        buttonsTable.row();
+        buttonsTable.add(highScoreButton).width(UI.Buttons.TEXT_BUTTON_WIDTH).height(UI.Buttons.TEXT_BUTTON_HEIGHT).pad(UI.Buttons.SPACE);
+        buttonsTable.row();
+        buttonsTable.setBackground(controlsBackground);
+
         innerTable.add(balloonImage).width(UI.Balloon.WIDTH).height(UI.Balloon.HEIGHT).pad(0, 0, -60, 0);
         innerTable.row();
-        innerTable.add(playButton).width(UI.Buttons.TEXT_BUTTON_WIDTH).height(UI.Buttons.TEXT_BUTTON_HEIGHT).pad(UI.Buttons.SPACE);
-        innerTable.row();
-        innerTable.add(settingsButton).width(UI.Buttons.TEXT_BUTTON_WIDTH).height(UI.Buttons.TEXT_BUTTON_HEIGHT).pad(UI.Buttons.SPACE);
-        innerTable.row();
-        innerTable.add(highScoreButton).width(UI.Buttons.TEXT_BUTTON_WIDTH).height(UI.Buttons.TEXT_BUTTON_HEIGHT).pad(UI.Buttons.SPACE);
-        innerTable.row();
+        innerTable.add(buttonsTable).pad(70, 70, 70, 70);
 
         outerTable.add(innerTable).colspan(2).expand();
+
+//        innerTable.debugAll();
 
         // Add listeners
         playButton.addListener(menuListener);
